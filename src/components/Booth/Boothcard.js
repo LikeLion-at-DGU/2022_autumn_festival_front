2898737fb5a7f4f97ef212ee16d69b8c5e8243e3
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-09-18 05:30:55 +0900

    [ADD]버튼

---
+++ src/components/Booth/Boothcard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Boothcard.css';
+
+export default function Boothcard({ title }) {
+  return (
+    <section className="row">
+      <h2>{title}</h2>
+    </section>
+  );
+}
+//스와이프모르게따
--- src/pages/Booth/Booth.js
+++ src/pages/Booth/Booth.js
@@ -1,9 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Booth.css';
+import Boothcard from '../../components/Booth/Boothcard';
+import requests from '../../api/requests';
+import CategoryFilter from '../../hooks/categoryFilter';
+
+//날짜 데이터 예시
+const Datecategories = [
+  {
+    name: '수요일',
+    day: '28일\n',
+    value: '28',
+  },
+  {
+    name: '목요일',
+    day: '29일\n',
+    value: '29',
+  },
+  {
+    name: '금요일',
+    day: '30일\n',
+    value: '30',
+  },
+];
 
 export default function Booth() {
   const [searchValue, setsearchValue] = useState('');
+
+  // 초기값 28일
+  const [category, setCategory] = useState('28');
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -28,6 +53,16 @@ export default function Booth() {
         src="https://velog.velcdn.com/images/seochan99/post/bfed67d9-30c2-4d59-ae59-7fa0d077618b/image.png"
         className="boothMap"
       />
+
+      <CategoryFilter
+        categories={Datecategories}
+        category={category}
+        setDateCategory={setCategory}
+      />
+
+      <hr />
+
+      <Boothcard title={requests.title} />
     </section>
   );
 }
