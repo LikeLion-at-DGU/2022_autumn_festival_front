57e37eed7ca972e41f4f5e1d648a424dad9a0203
Seulg2027 <seulgi2027@dgu.ac.kr>
2022-09-17 20:39:51 +0900

    라우터 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { GlobalStyle } from './styles/GlobalStyle';
 
 /* Internal libraries */
 import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Booth from './pages/Booth/Booth';
+import Notice from './pages/Notice/Notice';
+import TimeTable from './pages/TimeTable/TimeTable';
 
 function App() {
   return (
@@ -12,6 +16,10 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/booth" element={<Booth />} />
+          <Route path="/notice" element={<Notice />} />
+          <Route path="/timetable" element={<TimeTable />} />
         </Routes>
       </div>
     </>
