bf5d87a88d1cc106b900b7b3be3919b1fb156bcd
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-09-22 01:13:43 +0900

    aboutmenu수정

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -4,17 +4,27 @@ import dguLogo from '../../assets/img/dguLogo.png';
 import aboutFLogo from '../../assets/img/aboutFLogo.png';
 import aboutC from '../../assets/img/aboutC.png';
 import aboutFace from '../../assets/img/aboutFace.png';
+import LogoImg from '../../assets/img/maple.png';
 
 import { UpTitle } from '../../styles/style';
 
+const TitleContainer = styled.section`
+  position: relative;
+`;
+
+const Img = styled.img`
+  width: 45px;
+  height: 45px;
+  position: absolute;
+  bottom: 10px;
+  left: 87px;
+`;
 const Title = styled.h1`
-  font-family: 'Gmarket Sans TTF';
-  font-size: 100px;
-  text-shadow: 1px 1px 0.2px rgba(0, 0, 0, 0.25);
-  font-style: normal;
-  font-weight: 700;
+  font-weight: 800;
   font-size: 28px;
-  line-height: 32px;
+  line-height: 32.2px;
+  letter-spacing: 0.1em;
+  text-shadow: 1px 1px 0.2px rgba(0, 0, 0, 0.25);
 `;
 
 const LogoImage = styled.img`
@@ -143,8 +153,11 @@ const Space = styled.div`
 
 export default function About() {
   return (
-    <div>
-      <Title>🍁About</Title>
+    <div style={{ marginTop: '97rem' }}>
+      <TitleContainer>
+        <Img src={LogoImg} />
+        <Title>ABOUT</Title>{' '}
+      </TitleContainer>
 
       <Logo>
         <LogoImage src={dguLogo} />
