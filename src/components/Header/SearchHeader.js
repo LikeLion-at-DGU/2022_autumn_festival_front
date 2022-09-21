89734412b35214d15672c19f9a24183b75f3317c
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-09-21 16:59:44 +0900

    boothTitle수정

--- src/components/Header/SearchHeader.js
+++ src/components/Header/SearchHeader.js
@@ -1,8 +1,30 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../../pages/Booth/Booth.css';
-import maple from '../../assets/img/maple.png';
-import { display } from '@mui/system';
+import styled from 'styled-components';
+import LogoImg from '../../assets/img/maple.png';
+
+const TitleContainer = styled.section`
+  position: relative;
+  width: inherit;
+  margin-top: 100px; ;
+`;
+
+const Title = styled.h1`
+  font-weight: 800;
+  font-size: 28px;
+  line-height: 32.2px;
+  letter-spacing: 0.1em;
+  text-shadow: 1px 1px 0.2px rgba(0, 0, 0, 0.25);
+`;
+
+const Img = styled.img`
+  width: 45px;
+  height: 45px;
+  position: absolute;
+  bottom: 90px;
+  left: 117px;
+`;
 
 export default function SearchHeader() {
   const [searchValue, setsearchValue] = useState('');
@@ -20,25 +42,30 @@ export default function SearchHeader() {
     <div className="booth_page_header">
       {/* <img src={maple} style={{ width: '2rem' }} /> */}
 
-      <h1 className="booth_page_title">
-        <a
-          href="/booth"
-          style={{
-            textDecorationLine: 'none',
-            color: 'white',
-          }}
-        >
-          🍁부스
-        </a>
-      </h1>
+      <h1 className="booth_page_title"></h1>
       {/* 검색창 */}
-      <input
-        value={searchValue}
-        onChange={handleChange}
-        className="nav__input"
-        type="text"
-        placeholder="부스이름 또는 메뉴 검색"
-      />
+
+      <TitleContainer>
+        <Img src={LogoImg} />
+        <Title>
+          <a
+            href="/booth"
+            style={{
+              textDecorationLine: 'none',
+              color: 'white',
+            }}
+          >
+            부스
+          </a>
+        </Title>
+        <input
+          value={searchValue}
+          onChange={handleChange}
+          className="nav__input"
+          type="text"
+          placeholder="부스이름 또는 메뉴 검색"
+        />
+      </TitleContainer>
     </div>
   );
 }
