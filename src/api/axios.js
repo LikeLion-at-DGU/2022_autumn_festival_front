25325d531335aecda0d5f97d3de63192ba59312b
Seulg2027 <seulgi2027@dgu.ac.kr>
2022-09-25 02:24:44 +0900

    부스 백이랑 데이터 맞추는 중...

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080/api/',
+  baseURL: 'http://192.168.0.194:8080/api/',
+  headers: {
+    'Access-Control-Allow-Credentials': true,
+  },
 });
 
 export default instance;
