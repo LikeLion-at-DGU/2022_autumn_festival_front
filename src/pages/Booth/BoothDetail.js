f7231494f2a8bef416400b9c4f0ad541912e32f9
Seulg2027 <seulgi2027@dgu.ac.kr>
2022-09-24 22:46:18 +0900

    update: 부스 디테일 없을 때 예외처리

--- src/pages/Booth/BoothDetail.js
+++ src/pages/Booth/BoothDetail.js
@@ -33,6 +33,8 @@ import NotificationsIcon from '@mui/icons-material/Notifications';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import CancelIcon from '@mui/icons-material/Cancel';
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Pagination, Autoplay } from 'swiper';
@@ -55,7 +57,7 @@ export default function BoothDetail() {
     location: '명진관 9번',
     notice:
       '16일 우천시에도 운영합니다! <br/>*교공 하트키링 판매중(3000원, 한정수량)<br/><br/>[운영 시간] 10:00 - 16:00 (품절시 마감 공지)<br/>[운영 위치] 교육관 7번 부스<br/><br/>*교육관 생각보다 안멀어요!! (포관쪽에서 5분 소요) 드셔보세요ヾ(•ω•`)o*<br/><br/>**주인장의 꿀팁: 콘치즈랑 팝콘치킨을 모두 추가한 게 제일 맛있습니다(메인 메뉴임)**',
-    startAt: '2022-09-23',
+    days: [28, 29, 30],
     content:
       '맛있는 호떡 먹고 가세요~맛있는 호떡 먹고 가세요 피자, 슈크림, 등 다양한 맛을 판매중 입니다.~<br/>코로나 19를 딛고 다시 힘차게 오픈하게 된 저희 불닭볶음밥 전문점, 원조 「교테전 불닭볶음밥 식당」을 찾아주신 고객님들 환영합니다 *^^*<br/><br/>기본적으로 치즈가 듬뿍 올라가게 되어 맵지가 않으니 매운 음식을 마다하시는 분들도 맛나게 드실 수 있습니다.<br/>특히, 요 근래 유행하는 콘치즈를 얹어 먹으면 아이들에게도 인기만점! 한끼 식사거리가 될 수 있답니다.<br/><br/>여러분들께서도 공강시간에 배고프실 때, 든든히 먹을 수 있는 밥을 한끼 찾고 계시다면 우리 원조 「교테전 불닭볶음밥 식당」을 찾아주시기 바랍니다.',
     images: [
@@ -85,6 +87,7 @@ export default function BoothDetail() {
     },
   ]);
   const [lovecnt, setLovecnt] = useState(100);
+  const [isExist, setIsExist] = useState(true);
   let detailId = useParams().id;
 
   // toggle //
@@ -126,6 +129,17 @@ export default function BoothDetail() {
         style={{ fontSize: '28px' }}
       />
     );
+  };
+
+  const fetchBoothDetail = () => {
+    axios
+      .get(`booths/${detailId}`)
+      .then((res) => {
+        setBooth(res.data);
+      })
+      .catch((e) => {
+        setIsExist(false);
+      });
   };
 
   // 주점 소개 뷰 //
@@ -227,134 +241,161 @@ export default function BoothDetail() {
       })
       .catch((error) => console.log('Network Error : ', error));
   };
-  useEffect(getComments, []);
+
+  useEffect(() => {
+    getComments();
+    fetchBoothDetail();
+  }, []);
   return (
-    <div style={{ marginBottom: '76px' }}>
-      <UpTitle
-        title={`${booth.boothType.korean} 홈페이지`}
-        mapleLeft={booth.boothType.korean === '푸드트럭' ? '30px' : '57px'}
-      />
+    <>
+      {isExist ? (
+        <div style={{ marginBottom: '76px' }}>
+          <UpTitle
+            title={`${booth.boothType.korean} 홈페이지`}
+            mapleLeft={booth.boothType.korean === '푸드트럭' ? '30px' : '57px'}
+          />
 
-      {/* 스와이퍼 */}
-      <SwiperContainer>
-        <Swiper
-          className="banner"
-          spaceBetween={50}
-          slidesPerView={1}
-          pagination={{ clickable: true }}
-          autoplay={{ delay: 4200 }}
-          style={{ height: 360 }}
-        >
-          {SlideView}
-        </Swiper>
-      </SwiperContainer>
+          {/* 스와이퍼 */}
+          <SwiperContainer>
+            <Swiper
+              className="banner"
+              spaceBetween={50}
+              slidesPerView={1}
+              pagination={{ clickable: true }}
+              autoplay={{ delay: 4200 }}
+              style={{ height: 360 }}
+            >
+              {SlideView}
+            </Swiper>
+          </SwiperContainer>
 
-      {/* 부스 내용 */}
-      <ContentContainer>
-        <TypeBtn tp={booth.boothType.korean}>{booth.boothType.korean}</TypeBtn>
-        <BoothTitle>{booth.title}</BoothTitle>
-        <BoothIntro>{booth.introduction}</BoothIntro>
-        {admin === 'true' ? (
-          <EditBtn onClick={() => navigate(`/booth/${detailId}/edit`)}>
-            수정하기
-          </EditBtn>
-        ) : (
-          <></>
-        )}
-        <br />
+          {/* 부스 내용 */}
+          <ContentContainer>
+            <TypeBtn tp={booth.boothType.korean}>
+              {booth.boothType.korean}
+            </TypeBtn>
+            <BoothTitle>{booth.title}</BoothTitle>
+            <BoothIntro>{booth.introduction}</BoothIntro>
+            {admin === 'true' ? (
+              <EditBtn onClick={() => navigate(`/booth/${detailId}/edit`)}>
+                수정하기
+              </EditBtn>
+            ) : (
+              <></>
+            )}
+            <br />
 
-        <div style={{ display: 'flex', alignItem: 'center' }}>
-          {HeartView(booth.boothType.korean)}
-          &nbsp;
-          <LikeCnt>{lovecnt}</LikeCnt>
-        </div>
-
-        <DateLocContainer>
-          <div>{booth.startAt.slice(8, 10)}일</div>&nbsp;
-          <LocationMap>
-            <img src={MapIconImg} width="24" height="26" />
-            {booth.location}
-          </LocationMap>
-        </DateLocContainer>
-
-        {/* 부스 공지사항 */}
-        {noticeToggle ? (
-          <BoothNotificationOpen
-            onClick={() => {
-              setNoticeToggle(false);
-            }}
-          >
-            <div className="bar">
-              <NotificationsIcon
-                style={{ fontSize: '16px', margin: '6px 6px 0 14px' }}
-              />
-              <div>부스 공지사항</div>
-              <KeyboardArrowUpIcon style={{ margin: '3px 14px 6px auto' }} />
+            <div style={{ display: 'flex', alignItem: 'center' }}>
+              {HeartView(booth.boothType.korean)}
+              &nbsp;
+              <LikeCnt>{lovecnt}</LikeCnt>
             </div>
-            <div
-              className="notice"
-              dangerouslySetInnerHTML={{ __html: booth.notice }}
-            ></div>
-          </BoothNotificationOpen>
-        ) : (
-          <BoothNotification
-            onClick={() => {
-              setNoticeToggle(true);
-            }}
-          >
-            <NotificationsIcon
-              style={{ fontSize: '16px', margin: '6px 6px 0 14px' }}
-            />
-            <div>부스 공지사항</div>
-            <KeyboardArrowDownIcon style={{ margin: '3px 14px 6px auto' }} />
-          </BoothNotification>
-        )}
 
-        {/* 부스 주점 소개 */}
-        <IntroContainer>
-          <div className="introtitle">
-            <img src={BoothdetailC} width="36px" />
-            <span>주점 소개</span>
-          </div>
-          <IntroLine></IntroLine>
-          {IntroView()}
-        </IntroContainer>
+            <DateLocContainer>
+              <div>
+                {booth.days.map((d) => {
+                  return <span key={d}>{d}일 &nbsp;</span>;
+                })}
+              </div>
+              &nbsp;
+              <LocationMap>
+                <img src={MapIconImg} width="24" height="26" />
+                {booth.location}
+              </LocationMap>
+            </DateLocContainer>
 
-        {/* 부스 메뉴 소개 */}
-        <IntroContainer>
-          <div className="introtitle">
-            <img src={BoothdetailC} width="36px" />
-            <span>메뉴</span>
-          </div>
-          <IntroLine></IntroLine>
-          <BoothMenuAdd admin={admin} menu={menu} setMenu={setMenu} />
-          <MenuContainer>{MenuView}</MenuContainer>
-        </IntroContainer>
+            {/* 부스 공지사항 */}
+            {noticeToggle ? (
+              <BoothNotificationOpen
+                onClick={() => {
+                  setNoticeToggle(false);
+                }}
+              >
+                <div className="bar">
+                  <NotificationsIcon
+                    style={{ fontSize: '16px', margin: '6px 6px 0 14px' }}
+                  />
+                  <div>부스 공지사항</div>
+                  <KeyboardArrowUpIcon
+                    style={{ margin: '3px 14px 6px auto' }}
+                  />
+                </div>
+                <div
+                  className="notice"
+                  dangerouslySetInnerHTML={{ __html: booth.notice }}
+                ></div>
+              </BoothNotificationOpen>
+            ) : (
+              <BoothNotification
+                onClick={() => {
+                  setNoticeToggle(true);
+                }}
+              >
+                <NotificationsIcon
+                  style={{ fontSize: '16px', margin: '6px 6px 0 14px' }}
+                />
+                <div>부스 공지사항</div>
+                <KeyboardArrowDownIcon
+                  style={{ margin: '3px 14px 6px auto' }}
+                />
+              </BoothNotification>
+            )}
 
-        {/* 방명록 */}
-        <IntroContainer>
-          <div className="introtitle">
-            <img src={BoothdetailC} width="36px" />
-            <span>방명록</span>
-          </div>
-          <IntroLine></IntroLine>
-          <CommentInput onInsert={onInsert} />
+            {/* 부스 주점 소개 */}
+            <IntroContainer>
+              <div className="introtitle">
+                <img src={BoothdetailC} width="36px" />
+                <span>주점 소개</span>
+              </div>
+              <IntroLine></IntroLine>
+              {IntroView()}
+            </IntroContainer>
 
-          {comments.map((comment) => {
-            console.log(comment);
-            return (
-              <GuestBookItem
-                detailId={detailId}
-                id={comment.id}
-                writer={comment.writer}
-                password={comment.password}
-                content={comment.content}
-                createdDateTime={comment.createdDateTime}
-              />
-            );
-          })}
-        </IntroContainer>
-      </ContentContainer>
-    </div>
+            {/* 부스 메뉴 소개 */}
+            <IntroContainer>
+              <div className="introtitle">
+                <img src={BoothdetailC} width="36px" />
+                <span>메뉴</span>
+              </div>
+              <IntroLine></IntroLine>
+              <BoothMenuAdd admin={admin} menu={menu} setMenu={setMenu} />
+              <MenuContainer>{MenuView}</MenuContainer>
+            </IntroContainer>
+
+            {/* 방명록 */}
+            <IntroContainer>
+              <div className="introtitle">
+                <img src={BoothdetailC} width="36px" />
+                <span>방명록</span>
+              </div>
+              <IntroLine></IntroLine>
+              <CommentInput onInsert={onInsert} />
+
+              {comments.map((comment) => {
+                // console.log(comment);
+                return (
+                  <GuestBookItem
+                    detailId={detailId}
+                    id={comment.id}
+                    writer={comment.writer}
+                    password={comment.password}
+                    content={comment.content}
+                    createdDateTime={comment.createdDateTime}
+                  />
+                );
+              })}
+            </IntroContainer>
+          </ContentContainer>
+        </div>
+      ) : (
+        <>
+          <Stack sx={{ width: '328px', margin: '200px auto' }} spacing={2}>
+            <Alert variant="outlined" severity="warning" sx={{ color: '#fff' }}>
+              해당하는 게시글이 없습니다.
+            </Alert>
+          </Stack>
+        </>
+      )}
+    </>
   );
 }
