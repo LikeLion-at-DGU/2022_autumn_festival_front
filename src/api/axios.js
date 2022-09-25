aa9806695b831d16b675f4b47a63a9f01120dd02
Seulg2027 <seulgi2027@dgu.ac.kr>
2022-09-25 22:58:31 +0900

    좋아요 드디어..

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://192.168.0.194:8080/api/',
+  baseURL: 'http://localhost:8080/api/',
   headers: {
     'Access-Control-Allow-Credentials': true,
   },
